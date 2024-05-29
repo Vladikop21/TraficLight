46c1acd757a16c3e62b0877da1e28061a37e1a97
Vladdoriy <vladik.karpitskiy@gmail.com>
2024-05-29 16:19:39 +0300

    Initial Commit

---
+++ TraficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraficLight
+//
+//  Created by Vladdoriy on 29.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
